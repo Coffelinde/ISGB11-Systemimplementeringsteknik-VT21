4165cbde14ac90fc0111cf3f8f558ee65f7ffcb1
Peter Bellström <peter.bellstrom@kau.se>
2021-03-01 09:40:21 +0100

    Uppdaterat F1

---
+++ F1/ex7_startkod.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en" >
+	<head>
+		<meta charset="utf-8" />
+		<title>F1 Exempel 7</title>
+		<style>
+			img { width: 10%; height: 10%; }
+		</style>
+		
+	</head>
+	<body>
+		<div>
+
+			<?php
+				echo ("<pre>" );
+				print_r( $_GET );
+				echo( "</pre>" );
+			?>
+			
+			<?php echo( $_SERVER["PHP_SELF"] ); ?>
+			
+			<a href="ex7_startkod.php?linkSend=true&linkTest=Alltid med...">Send</a>
+			<a href="ex7_startkod.php?linkDemo=true&linkTest=alltid med...">Demonstraion</a>
+		
+			<?php
+				//Här kommer koden...
+			?>
+			
+		</div>
+	</body>
+</html>
