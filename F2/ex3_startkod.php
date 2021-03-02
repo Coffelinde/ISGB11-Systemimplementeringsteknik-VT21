927c9ef6da35c28d4fea0a23d939bfaa2e43f06d
Peter Bellström <peter.bellstrom@kau.se>
2021-03-02 09:07:44 +0100

    Uppdaterat F2

---
+++ F2/ex3_startkod.php
@@ -0,0 +1,50 @@
+<?php
+
+	//Här skriver ni koden!
+			
+?>
+<!doctype html>
+<html lang="en" >
+	<head>
+		<meta charset="utf-8" />
+		<title>Ett exempel med kakor</title>
+		<style>
+			<?php
+				//Skriv ut CSS-instruktionerna...
+				if( isset( $css ) ) {
+					echo($css);
+				}
+			?>
+		</style>
+	</head>
+	<body>
+		<div>
+			
+			<form action="<?php echo( $_SERVER["PHP_SELF"] ); ?>" method="post">
+		
+				<input type="color" name="backgroundcolor" value="<?php if( isset( $bgColor )) { echo( $bgColor ); } ?>" />
+				<input type="color" name="foregroundcolor" value="<?php if( isset( $fgColor )) { echo( $fgColor ); } ?>"/>
+
+				<input type="submit" name="btnSend" value="Send" />
+				<input type="submit" name="btnReset" value="Reset" <?php if($disabled) { echo("disabled"); } ?>/>
+			
+			</form>
+		
+			<?php
+			
+				echo("<p>\$_POST</p>") . PHP_EOL;
+				echo( "<pre>" );
+				print_r( $_POST );
+				echo( "</pre>" );
+
+				echo("<p> \$_SESSION</p>") . PHP_EOL;
+				echo( "<pre>" );
+				print_r( $_COOKIE );
+				echo( "</pre>" );
+				
+				
+			?>
+			
+		</div>
+	</body>
+</html>
